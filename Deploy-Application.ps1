545844471595ae3b7880ac77c4592037d10b89d4
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2017-06-05 16:30:25 -0600

    Added ClickOnce uninstaller

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -115,33 +115,42 @@ Try {
 		Show-InstallationWelcome -CloseApps 'fusioncontact360' -CheckDiskSpace -PersistPrompt
 
 		## <Perform Pre-Installation tasks here>
-		## Remove previous versions
-		Remove-File -Path "$envSystemDrive\CallCenterApp" -Recurse
-		Remove-File -Path "$envSystemDrive\testFusionAll" -Recurse
-		Remove-File -Path "$envSystemDrive\Fusion360Installer" -Recurse
-		Remove-File -Path "$envSystemDrive\Fusion" -Recurse
-		Remove-File -Path "$envCommonDesktop\Fusion Contact360.appref-ms"
+		## Remove previous installations
+		Copy-File -Path "$dirSupportFiles\ClickOnceUninstaller.exe" -Destination "$envWinDir\MSUDenver"
+		Execute-Process -Path "$envWinDir\MSUDenver\ClickOnceUninstaller.exe" -Parameters "`"Fusion Contact360`"" -WindowStyle "Hidden"
+		Execute-Process -Path "$envWinDir\MSUDenver\ClickOnceUninstaller.exe" -Parameters "`"Fusion Contact360 - 1 `"" -WindowStyle "Hidden"
+
+		Remove-File -Path "$envCommonDesktop\Fusion Contact360*.appref-ms"
 		Remove-Folder -Path "$envSystemDrive\CallCenterApp"
 		Remove-Folder -Path "$envSystemDrive\testFusionAll"
 		Remove-Folder -Path "$envSystemDrive\Fusion360Installer"
 		Remove-Folder -Path "$envSystemDrive\Fusion"
-		##Remove shortcut
+		$exitCode = Remove-MSIApplications -Name "Fusion Agent Desktop" -AddParameters "ALLUSERS=1" -PassThru
+		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+
+		## Remove shortcuts and per-user installations
 		Remove-File -Path "$envCommonDesktop\CallCenterApp.lnk"
 
-		[scriptblock]$HKCURegistrySettings = {
-			Remove-RegistryKey -Key 'HKCU\Software\CaseWare IDEA' -Recurse -SID $UserProfile.SID
-			Set-RegistryKey -Key 'HKCU\Software\CaseWare IDEA\CaseWare IDEA\Config' -Name 'NextUpgradeCheck' -Value '0' -SID $UserProfile.SID
-			Set-RegistryKey -Key 'HKCU\Software\CaseWare IDEA\CaseWare IDEA\Config' -Name 'DisplayIntroPage' -Value '0' -Type 'DWord' -SID $UserProfile.SID
-		}
-
-		Get-ChildItem -Path "${envSystemDrive}\Users" -Force | Where-Object {$_.PSIsContainer -eq $True} | ForEach-Object {
-			$IDEAFolder = $_.FullName + "\Documents\My IDEA Documents"
-			If ((Test-Path $IDEAFolder) -eq $true) {
-				Remove-Folder $IDEAFolder
+		Get-ChildItem -Path "$envSystemDrive\Users" -Force | Where-Object {$_.PSIsContainer -eq $true} | ForEach-Object {
+			$FusionStartMenu = $_.FullName + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Fusion"
+			$FusionDesktopClickOnce = $_.FullName + "\Desktop\Fusion Contact360.appref-ms"
+			If ((Test-Path $FusionStartMenu) -eq $true) {
+				Remove-Folder $FusionStartMenu
+			}
+			If ((Test-Path $FusionDesktopClickOnce) -eq $true) {
+				Remove-File $FusionDesktopClickOnce
 			}
 		}
 
+		## Remove registry entries
+		[scriptblock]$HKCURegistrySettings = {
+			Remove-RegistryKey -Key "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "Fusion360Install" -SID $UserProfile.SID
+		}
+		Invoke-HKCURegistrySettingsForAllUsers -RegistrySettings $HKCURegistrySettings
 
+		## Uninstall ClickOnce application on next login
+		Set-ActiveSetup -StubExePath "$envWinDir\MSUDenver\ClickOnceUninstaller.exe" -Arguments "`"Fusion Contact360`"" -Description "Fusion Contact360" -Key "Fusion Contact360 Uninstall" -Version "1"
+		Set-ActiveSetup -StubExePath "$envWinDir\MSUDenver\ClickOnceUninstaller.exe" -Arguments "`"Fusion Contact360 - 1 `"" -Description "Fusion Contact360" -Key "Fusion Contact360 Uninstall 2" -Version "1"
 
 		##*===============================================
 		##* INSTALLATION
@@ -155,7 +164,7 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		$exitCode = Execute-MSI -Action "Install" -Path "FusionAgentDesktopSetup.msi" -Parameters "REBOOT=ReallySuppress /QN" -PassThru
+		$exitCode = Execute-MSI -Action "Install" -Path "FusionAgentDesktopSetup.msi" -Parameters "ALLUSERS=1 REBOOT=ReallySuppress /QN" -PassThru
 		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 		##*===============================================
 		##* POST-INSTALLATION
@@ -196,7 +205,7 @@ Try {
 		}
 
 		# <Perform Uninstallation tasks here>
-		$exitCode = Execute-MSI -Action "Uninstall" -Path "FusionAgentDesktopSetup.msi" -PassThru
+		$exitCode = Execute-MSI -Action "Uninstall" -Path "FusionAgentDesktopSetup.msi" -AddParameters "ALLUSERS=1" -PassThru
 		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 
 		##*===============================================
@@ -223,3 +232,118 @@ Catch {
 	Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
 	Exit-Script -ExitCode $mainExitCode
 }
+
+# SIG # Begin signature block
+# MIIU4wYJKoZIhvcNAQcCoIIU1DCCFNACAQExDzANBglghkgBZQMEAgEFADB5Bgor
+# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
+# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCAbm3Nsx7I2xIcE
+# +hjh00lky1LxL2VKmMXxFmXyvUzHTaCCD4cwggQUMIIC/KADAgECAgsEAAAAAAEv
+# TuFS1zANBgkqhkiG9w0BAQUFADBXMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xv
+# YmFsU2lnbiBudi1zYTEQMA4GA1UECxMHUm9vdCBDQTEbMBkGA1UEAxMSR2xvYmFs
+# U2lnbiBSb290IENBMB4XDTExMDQxMzEwMDAwMFoXDTI4MDEyODEyMDAwMFowUjEL
+# MAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExKDAmBgNVBAMT
+# H0dsb2JhbFNpZ24gVGltZXN0YW1waW5nIENBIC0gRzIwggEiMA0GCSqGSIb3DQEB
+# AQUAA4IBDwAwggEKAoIBAQCU72X4tVefoFMNNAbrCR+3Rxhqy/Bb5P8npTTR94ka
+# v56xzRJBbmbUgaCFi2RaRi+ZoI13seK8XN0i12pn0LvoynTei08NsFLlkFvrRw7x
+# 55+cC5BlPheWMEVybTmhFzbKuaCMG08IGfaBMa1hFqRi5rRAnsP8+5X2+7UulYGY
+# 4O/F69gCWXh396rjUmtQkSnF/PfNk2XSYGEi8gb7Mt0WUfoO/Yow8BcJp7vzBK6r
+# kOds33qp9O/EYidfb5ltOHSqEYva38cUTOmFsuzCfUomj+dWuqbgz5JTgHT0A+xo
+# smC8hCAAgxuh7rR0BcEpjmLQR7H68FPMGPkuO/lwfrQlAgMBAAGjgeUwgeIwDgYD
+# VR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFEbYPv/c
+# 477/g+b0hZuw3WrWFKnBMEcGA1UdIARAMD4wPAYEVR0gADA0MDIGCCsGAQUFBwIB
+# FiZodHRwczovL3d3dy5nbG9iYWxzaWduLmNvbS9yZXBvc2l0b3J5LzAzBgNVHR8E
+# LDAqMCigJqAkhiJodHRwOi8vY3JsLmdsb2JhbHNpZ24ubmV0L3Jvb3QuY3JsMB8G
+# A1UdIwQYMBaAFGB7ZhpFDZfKiVAvfQTNNKj//P1LMA0GCSqGSIb3DQEBBQUAA4IB
+# AQBOXlaQHka02Ukx87sXOSgbwhbd/UHcCQUEm2+yoprWmS5AmQBVteo/pSB204Y0
+# 1BfMVTrHgu7vqLq82AafFVDfzRZ7UjoC1xka/a/weFzgS8UY3zokHtqsuKlYBAIH
+# MNuwEl7+Mb7wBEj08HD4Ol5Wg889+w289MXtl5251NulJ4TjOJuLpzWGRCCkO22k
+# aguhg/0o69rvKPbMiF37CjsAq+Ah6+IvNWwPjjRFl+ui95kzNX7Lmoq7RU3nP5/C
+# 2Yr6ZbJux35l/+iS4SwxovewJzZIjyZvO+5Ndh95w+V/ljW8LQ7MAbCOf/9RgICn
+# ktSzREZkjIdPFmMHMUtjsN/zMIIEnzCCA4egAwIBAgISESHWmadklz7x+EJ+6RnM
+# U0EUMA0GCSqGSIb3DQEBBQUAMFIxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i
+# YWxTaWduIG52LXNhMSgwJgYDVQQDEx9HbG9iYWxTaWduIFRpbWVzdGFtcGluZyBD
+# QSAtIEcyMB4XDTE2MDUyNDAwMDAwMFoXDTI3MDYyNDAwMDAwMFowYDELMAkGA1UE
+# BhMCU0cxHzAdBgNVBAoTFkdNTyBHbG9iYWxTaWduIFB0ZSBMdGQxMDAuBgNVBAMT
+# J0dsb2JhbFNpZ24gVFNBIGZvciBNUyBBdXRoZW50aWNvZGUgLSBHMjCCASIwDQYJ
+# KoZIhvcNAQEBBQADggEPADCCAQoCggEBALAXrqLTtgQwVh5YD7HtVaTWVMvY9nM6
+# 7F1eqyX9NqX6hMNhQMVGtVlSO0KiLl8TYhCpW+Zz1pIlsX0j4wazhzoOQ/DXAIlT
+# ohExUihuXUByPPIJd6dJkpfUbJCgdqf9uNyznfIHYCxPWJgAa9MVVOD63f+ALF8Y
+# ppj/1KvsoUVZsi5vYl3g2Rmsi1ecqCYr2RelENJHCBpwLDOLf2iAKrWhXWvdjQIC
+# KQOqfDe7uylOPVOTs6b6j9JYkxVMuS2rgKOjJfuv9whksHpED1wQ119hN6pOa9PS
+# UyWdgnP6LPlysKkZOSpQ+qnQPDrK6Fvv9V9R9PkK2Zc13mqF5iMEQq8CAwEAAaOC
+# AV8wggFbMA4GA1UdDwEB/wQEAwIHgDBMBgNVHSAERTBDMEEGCSsGAQQBoDIBHjA0
+# MDIGCCsGAQUFBwIBFiZodHRwczovL3d3dy5nbG9iYWxzaWduLmNvbS9yZXBvc2l0
+# b3J5LzAJBgNVHRMEAjAAMBYGA1UdJQEB/wQMMAoGCCsGAQUFBwMIMEIGA1UdHwQ7
+# MDkwN6A1oDOGMWh0dHA6Ly9jcmwuZ2xvYmFsc2lnbi5jb20vZ3MvZ3N0aW1lc3Rh
+# bXBpbmdnMi5jcmwwVAYIKwYBBQUHAQEESDBGMEQGCCsGAQUFBzAChjhodHRwOi8v
+# c2VjdXJlLmdsb2JhbHNpZ24uY29tL2NhY2VydC9nc3RpbWVzdGFtcGluZ2cyLmNy
+# dDAdBgNVHQ4EFgQU1KKESjhaGH+6TzBQvZ3VeofWCfcwHwYDVR0jBBgwFoAURtg+
+# /9zjvv+D5vSFm7DdatYUqcEwDQYJKoZIhvcNAQEFBQADggEBAI+pGpFtBKY3IA6D
+# lt4j02tuH27dZD1oISK1+Ec2aY7hpUXHJKIitykJzFRarsa8zWOOsz1QSOW0zK7N
+# ko2eKIsTShGqvaPv07I2/LShcr9tl2N5jES8cC9+87zdglOrGvbr+hyXvLY3nKQc
+# MLyrvC1HNt+SIAPoccZY9nUFmjTwC1lagkQ0qoDkL4T2R12WybbKyp23prrkUNPU
+# N7i6IA7Q05IqW8RZu6Ft2zzORJ3BOCqt4429zQl3GhC+ZwoCNmSIubMbJu7nnmDE
+# Rqi8YTNsz065nLlq8J83/rU9T5rTTf/eII5Ol6b9nwm8TcoYdsmwTYVQ8oDSHQb1
+# WAQHsRgwggbIMIIFsKADAgECAhN/AAAAIhO6jvua86/0AAEAAAAiMA0GCSqGSIb3
+# DQEBCwUAMGIxEzARBgoJkiaJk/IsZAEZFgNlZHUxGTAXBgoJkiaJk/IsZAEZFglt
+# c3VkZW52ZXIxFTATBgoJkiaJk/IsZAEZFgV3aW5hZDEZMBcGA1UEAxMQd2luYWQt
+# Vk1XQ0EwMS1DQTAeFw0xNjA1MjcyMTI0MDJaFw0xODA1MjcyMTI0MDJaMIG/MQsw
+# CQYDVQQGEwJVUzERMA8GA1UECBMIQ29sb3JhZG8xDzANBgNVBAcTBkRlbnZlcjEw
+# MC4GA1UEChMnTWV0cm9wb2xpdGFuIFN0YXRlIFVuaXZlcnNpdHkgb2YgRGVudmVy
+# MSgwJgYDVQQLEx9JbmZvcm1hdGlvbiBUZWNobm9sb2d5IFNlcnZpY2VzMTAwLgYD
+# VQQDEydNZXRyb3BvbGl0YW4gU3RhdGUgVW5pdmVyc2l0eSBvZiBEZW52ZXIwggEi
+# MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCxCPUOmGXq89WCOBso0z5QIApw
+# EosnzQeoI9zP+n8wEb7BEA//+UTmjIZHe3jP0dF6C7EFhx2FcZxs8XQgSH5bnwor
+# rkLMa1FzcP2GlcNE5F+ms1zk5Bp2x2nsMOcx+12h9A6eU+JR3nXfWFwkNfvOAKrj
+# 1mo4BO5TEvx4DtrVBYFli+0JGnALa1Hd7A68nYtG743FPbioQn8EQSnDr+Jjtd8l
+# vujd9I5IQPptiU3inmcoaG+UFz8HKu7QS/mOLpoz/kjbSShxdNF0mcFmowg8WYMu
+# f8f1trOtsmWJ3lpyroKek8Ie9oOnKw3And2dOgqWxVXnfLEhW8b6PElvZc73AgMB
+# AAGjggMXMIIDEzAOBgNVHQ8BAf8EBAMCBaAwEwYDVR0lBAwwCgYIKwYBBQUHAwEw
+# GwYJKwYBBAGCNxUKBA4wDDAKBggrBgEFBQcDAzAdBgNVHQ4EFgQUxu8skV6twX8T
+# i5hj8XjbzTUYeqgwHwYDVR0jBBgwFoAUbmigb8ibDuAf063cjbVhC57XDzQwggEo
+# BgNVHR8EggEfMIIBGzCCARegggEToIIBD4aBxWxkYXA6Ly8vQ049d2luYWQtVk1X
+# Q0EwMS1DQSgxKSxDTj1WTVdDQTAxLENOPUNEUCxDTj1QdWJsaWMlMjBLZXklMjBT
+# ZXJ2aWNlcyxDTj1TZXJ2aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPXdpbmFkLERD
+# PW1zdWRlbnZlcixEQz1lZHU/Y2VydGlmaWNhdGVSZXZvY2F0aW9uTGlzdD9iYXNl
+# P29iamVjdENsYXNzPWNSTERpc3RyaWJ1dGlvblBvaW50hkVodHRwOi8vVk1XQ0Ew
+# MS53aW5hZC5tc3VkZW52ZXIuZWR1L0NlcnRFbnJvbGwvd2luYWQtVk1XQ0EwMS1D
+# QSgxKS5jcmwwggE+BggrBgEFBQcBAQSCATAwggEsMIG6BggrBgEFBQcwAoaBrWxk
+# YXA6Ly8vQ049d2luYWQtVk1XQ0EwMS1DQSxDTj1BSUEsQ049UHVibGljJTIwS2V5
+# JTIwU2VydmljZXMsQ049U2VydmljZXMsQ049Q29uZmlndXJhdGlvbixEQz13aW5h
+# ZCxEQz1tc3VkZW52ZXIsREM9ZWR1P2NBQ2VydGlmaWNhdGU/YmFzZT9vYmplY3RD
+# bGFzcz1jZXJ0aWZpY2F0aW9uQXV0aG9yaXR5MG0GCCsGAQUFBzAChmFodHRwOi8v
+# Vk1XQ0EwMS53aW5hZC5tc3VkZW52ZXIuZWR1L0NlcnRFbnJvbGwvVk1XQ0EwMS53
+# aW5hZC5tc3VkZW52ZXIuZWR1X3dpbmFkLVZNV0NBMDEtQ0EoMSkuY3J0MCEGCSsG
+# AQQBgjcUAgQUHhIAVwBlAGIAUwBlAHIAdgBlAHIwDQYJKoZIhvcNAQELBQADggEB
+# AIpoMvUtE1iFHSbi7X/M9a+JBPpiAQZzEbq70is1mzdosSVTMN7QoWk4WzHCJBpX
+# Oh7cvBrTLf0m4EqJ7OwPY43ZW7MycOjgtk393CaCzr9BiEDjWzJf8r5bDDCodEFm
+# dodj3/el8nV4HapjiGnJKrhg0b3xRjPP4cvjtBltbqO7tngkpDu+m63X68aC3wrt
+# XwJulfsGeTbd0v4hkji9GCTpLT92mkJyJE04SA/thv4F7yNx1W5XCEWswZeGLiR5
+# 9C5AlUm1WrhjAaoyxabDJWfljV//qk+TeoC5CNQ7ZkqdxFBYPc5d2UdkmmiK76D+
+# qaobXtlVJ9wRYfFoOaUb5dQxggSyMIIErgIBATB5MGIxEzARBgoJkiaJk/IsZAEZ
+# FgNlZHUxGTAXBgoJkiaJk/IsZAEZFgltc3VkZW52ZXIxFTATBgoJkiaJk/IsZAEZ
+# FgV3aW5hZDEZMBcGA1UEAxMQd2luYWQtVk1XQ0EwMS1DQQITfwAAACITuo77mvOv
+# 9AABAAAAIjANBglghkgBZQMEAgEFAKBmMBgGCisGAQQBgjcCAQwxCjAIoAKAAKEC
+# gAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwLwYJKoZIhvcNAQkEMSIEIIKV
+# N0KO+osEQEt6aqTWHoRet05DmJY8ajumarlhXA0AMA0GCSqGSIb3DQEBAQUABIIB
+# AIkCSx8tU/LNKpKfQqHIqKwFEjX3/ttBGkMtxVALfV9OaoA9kfbpe7Fs/x1ZPQGV
+# 7aQQCvN/R63AgVIoXLpKOyor0Se8rtUedeLL1sNeJNRFWtg1x2qjGRYltUnDJ3n6
+# t+6s4fWdMSS9184WYt7+BcXwe6wscUaIu/VRiUcEfCjK0by7BHd8vHjoviOE7jhN
+# FEpS1pSSkHif+7cth2tdbLAjLfC2bSykTbW/B6kkWay3HLwxvnLvEBPYBg1gRjUb
+# A/+ON7jAJa9lNCbClxIuEA6VvNqAlDLQikkBQuxhe5ocau5S+AQvBfH84ksW4rvL
+# whMCiCUDZi0AbpqYBquTNh+hggKiMIICngYJKoZIhvcNAQkGMYICjzCCAosCAQEw
+# aDBSMQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBudi1zYTEoMCYG
+# A1UEAxMfR2xvYmFsU2lnbiBUaW1lc3RhbXBpbmcgQ0EgLSBHMgISESHWmadklz7x
+# +EJ+6RnMU0EUMAkGBSsOAwIaBQCggf0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEH
+# ATAcBgkqhkiG9w0BCQUxDxcNMTcwNjA1MjIyOTU4WjAjBgkqhkiG9w0BCQQxFgQU
+# wBZXiNGDUFnHrPAu5Ptk3gZzFwEwgZ0GCyqGSIb3DQEJEAIMMYGNMIGKMIGHMIGE
+# BBRjuC+rYfWDkJaVBQsAJJxQKTPseTBsMFakVDBSMQswCQYDVQQGEwJCRTEZMBcG
+# A1UEChMQR2xvYmFsU2lnbiBudi1zYTEoMCYGA1UEAxMfR2xvYmFsU2lnbiBUaW1l
+# c3RhbXBpbmcgQ0EgLSBHMgISESHWmadklz7x+EJ+6RnMU0EUMA0GCSqGSIb3DQEB
+# AQUABIIBAEaz3e6NSycvJysF5qhFA0/UG8Esl83Cbgn04ivjlCckshIOJIOXGYr3
+# NmzNSOOavYjEkR52P6u5/Co5lDLGKwDs6bibChlFZdz0mTYjeig+54tq1Zk7lNxM
+# oCzfRx5X0YqjJfJdpVpyddysQc44t9Q+5KYSIccU9kZQSbu6Ts7BZY1hgNt+3ZO5
+# J7MyseiULjAqmzw2+w837aWD6TlBTTi16F7AP4D0PYqRpEllKVDbqGW71ikC/OmS
+# 6/Mlya29sybYOKCYXUL1A1lN4pQmmV95wUWpJq2tBLyzSGttfiI8QqD67JwjUvng
+# j1DMEwxDG1NZw2VyV32HaDu3s6oN3eY=
+# SIG # End signature block
